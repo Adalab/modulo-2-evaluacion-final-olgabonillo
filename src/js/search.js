ff8d2673e87b648ff8389c54094ab925e71893cb
Olga <olgaalaquas@gmail.com>
2024-07-16 14:17:02 +0200

    created new array

--- src/js/search.js
+++ src/js/search.js
@@ -5,6 +5,7 @@ const inputSearch = document.querySelector(".js-tittle-search");
 const button = document.querySelector(".js-button");
 const urlApi = "https://api.jikan.moe/v4/anime?q="
 const films = document.querySelector(".js-films");
+const favoritas = []
 
 
 //EVENTOS
@@ -23,19 +24,19 @@ button.addEventListener("click", (event) => {
         console.log("esto es lo que tiene object: ",object);
         console.log("esto es lo que tiene object.data: ", object.data);
 
-        //Reseteo la sección films
+        //Genero la sección films
         films.innerHTML = 
-        `<div class="results"><h2>Resultados:</h2></div>
-        <div class="favorites-series>Series favoritas:</div>`;
+        `<div class="js-results"><h2>Resultados:</h2></div><div class="js-favorites-series"><h2>Series favoritas:</h2></div>`;
         //Genero una constante para las películas que no tienen imagen
         const imageTv = "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png";
         const placeHolder = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
-
+        const favorites = document.querySelector(".js-favorites-series");
+        const results = document.querySelector(".js-results");
         object.data.forEach(function(data) {
             //Recojo información de cada uno de los títulos 
-            const imageUrl = data.images.webp.image_url
-            const title = data.title
-
+            const imageUrl = data.images.webp.image_url;
+            const title = data.title;
+            const id = data.mal_id;
             if (imageUrl === imageTv){
                 imageUrl = placeHolder;
             }
@@ -43,14 +44,29 @@ button.addEventListener("click", (event) => {
             console.log("este es el titulo de la pelicula: ", title);
             
             //Añado la información de la película al HTML
-            films.innerHTML += `<div class="js-film-card"><img src="${imageUrl}"/><h3>${title}</h3></div>`
+            results.innerHTML += `<div class="js-film-card" data-id="${id}"><img src="${imageUrl}"/><h3>${title}</h3></div>`
         });
         const favoriteSeries = document.querySelector(".js-film-card");
         //Función ejecutada cuando la usuaria hace click en la imagen de sus series favoritas
         const handleClickSeries = (event) => {
             event.preventDefault();
-            console.log("La película favorita de la usuaria es: ", event.currentTarget);
+            console.log("esto es lo que contiene event: ", event );
+            console.log("La película favorita de la usuaria es: ", event.currentTarget.getAttribute("data-id"));
             console.log("La película favorita de la usuaria es: ", event.target);
+            // Añadir el currentTarget a la array de favoritos
+            /* favoritas.push({
+                'id': id,
+                'title': title,
+                'imageUrl': imageUrl
+            }) */
+           /* arraySeriesFavoritas = [
+                {
+                    'id': id,
+                    'title': title,
+                    'imageUrl': imageUrl
+                }
+            ]*/
+
         }
         favoriteSeries.addEventListener("click", handleClickSeries);
     });
