c23d6b359b76a3c7db0d650abda50d0535451b1b
Olga <olgaalaquas@gmail.com>
2024-07-16 13:15:46 +0200

    created new functions

--- src/js/search.js
+++ src/js/search.js
@@ -6,6 +6,7 @@ const button = document.querySelector(".js-button");
 const urlApi = "https://api.jikan.moe/v4/anime?q="
 const films = document.querySelector(".js-films");
 
+
 //EVENTOS
 //Función ejecutada cuando el usuario hace click en el botón
 button.addEventListener("click", (event) => {
@@ -23,25 +24,34 @@ button.addEventListener("click", (event) => {
         console.log("esto es lo que tiene object.data: ", object.data);
 
         //Reseteo la sección films
-        films.innerHTML = "";
-
+        films.innerHTML = 
+        `<div class="results"><h2>Resultados:</h2></div>
+        <div class="favorites-series>Series favoritas:</div>`;
         //Genero una constante para las películas que no tienen imagen
         const imageTv = "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png";
         const placeHolder = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
 
         object.data.forEach(function(data) {
             //Recojo información de cada uno de los títulos 
-            const title = data.title
             const imageUrl = data.images.webp.image_url
-            
+            const title = data.title
+
             if (imageUrl === imageTv){
                 imageUrl = placeHolder;
             }
-            console.log("este es el titulo de la pelicula: ", title);
             console.log("esta es la imagen de la pelicula: ", imageUrl);
+            console.log("este es el titulo de la pelicula: ", title);
             
             //Añado la información de la película al HTML
-            films.innerHTML += `<div class="film-card"><h3>${title}</h3><img src="${imageUrl}"/></div>`
+            films.innerHTML += `<div class="js-film-card"><img src="${imageUrl}"/><h3>${title}</h3></div>`
         });
+        const favoriteSeries = document.querySelector(".js-film-card");
+        //Función ejecutada cuando la usuaria hace click en la imagen de sus series favoritas
+        const handleClickSeries = (event) => {
+            event.preventDefault();
+            console.log("La película favorita de la usuaria es: ", event.currentTarget);
+            console.log("La película favorita de la usuaria es: ", event.target);
+        }
+        favoriteSeries.addEventListener("click", handleClickSeries);
     });
-  });
+});
